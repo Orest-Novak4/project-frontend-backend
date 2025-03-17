a83530a25b5458171d38ea45870e530178a781d3
NazarMakara <nazar46464646@gmail.com>
2025-03-17 22:00:30 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function navigateTo(page) {
     window.location.href = page;
 }
+function showRegistrationAlert() {
+    alert("Before using the site, please register by clicking the 'Join Now' button.");
+}
+
+function navigateTo(url) {
+    window.location.href = url;
+}
+
